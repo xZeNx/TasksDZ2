728b7be61964282a9e94019528032640b826a678
ZeN <zensmi@yandex.ru>
2022-08-13 15:40:55 +0300

    DZ

---
+++ DZ1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Число: {number} - Является палиндромом.");
+  }
+  else Console.WriteLine($"Число: {number} - НЕ является палиндромом.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите правильное число.");
+
