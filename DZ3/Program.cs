46d1194cc671d028ba1039c9746acd1dc6a71ad2
ZeN <zensmi@yandex.ru>
2022-08-13 16:07:35 +0300

    DZ

---
+++ DZ3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+int number = ReadInt("Введите число: ");
+int i = 1;
+
+while(i <= number)
+{
+    Console.Write(Sqr(i) + " ");
+    i++;
+}
+
+int Sqr(int a)
+{
+    return a * a * a;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
